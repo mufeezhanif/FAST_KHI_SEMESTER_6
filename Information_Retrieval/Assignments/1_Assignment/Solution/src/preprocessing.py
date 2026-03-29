09fe448aacf4a42105da500fb4e98e62692c80ab
mufeezhanif <mufeez.hanif123@gmail.com>
2026-03-29 18:03:35 +0500

    ir assignment 1 completed

--- Information_Retrieval/Assignments/1_Assignment/Solution/src/preprocessing.py
+++ Information_Retrieval/Assignments/1_Assignment/Solution/src/preprocessing.py
@@ -1,4 +1,12 @@
 from typing import List, Set
+from nltk.stem import PorterStemmer
+
+_porter = PorterStemmer()
+
+
+def porter_stem_word(word: str) -> str:
+    return _porter.stem(word)
+
 
 def load_stopwords(path: str) -> Set[str]:
     stopwords = set()
@@ -26,8 +34,42 @@ def tokenize(text:str)-> List[str]:
         
     return tokens
 
-def stem_word(word: str)-> str:
+def basic_stem_word(word: str)-> str:
     if len(word)<=3:
         return word
     
-    rules 
+    rules = [
+        ("ingly", ""),
+        ("edly", ""),
+        ("ing", ""),
+        ("ed", ""),
+        ("ies", "y"),
+        ("sses", "ss"),
+        ("s", ""),
+        ("ly", ""),
+        ("ment", ""),
+    ]
+    
+    for suffix, replacement in rules:
+        if word.endswith(suffix)  and len(word) > len(suffix) +2:
+            return word[: -len(suffix)] + replacement
+        
+    return word
+
+def normalize_tokens(tokens: List[str], stopwords: Set[str], stem_mode: str = "porter")->List[str]:
+    cleaned = []
+    
+    if stem_mode == 'porter':
+        stem_fn = porter_stem_word
+    else:
+        stem_fn = basic_stem_word
+        
+    for tok in tokens:
+        if tok in stopwords:
+            continue
+        cleaned.append(stem_fn(tok))
+    return cleaned
+
+def preprocess_text(text: str, stopwords: Set[str], stem_mode:str='porter') -> List[str]:
+    tokens = tokenize(text)
+    return normalize_tokens(tokens, stopwords, stem_mode=stem_mode)
